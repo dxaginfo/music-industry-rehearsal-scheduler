3962769f2cd26d480de5d1151e6dcfd1fdc45f33
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-26 22:37:38 -0300

    Initial front-end file

---
+++ frontend/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>Rehearsal Scheduler</h1>
+      <p>Web app to schedule band rehearsals, attendance, reminders, and optimal time suggestions.</p>
+    </div>
+  );
+}
+export default App;
